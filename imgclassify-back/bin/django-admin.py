a1836e6d93a0a729352170288f637c038ec27355
Boulaioune, Yacine <yacineb2@illinois.edu>
2022-01-13 08:14:37 +0100

    added classififer component

--- imgclassify-back/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/yb97/Desktop/whatimage/imgclassify-back/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
--- imgclassify-front/src/App.js
+++ imgclassify-front/src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
+import Classifier from './components/Classifier/Classifier';
+import {Route, BrowserRouter, Routes} from 'react-router-dom'
 
 function App() {
   return (
       <div className='App'>
-       <h1>Hello World </h1>
+     
+     <Classifier/>
+      
       </div>
    
   );
