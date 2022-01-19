1fde3711496bf9abf9c3724bbd3d0d3e2b22eb8b
Boulaioune, Yacine <yacineb2@illinois.edu>
2022-01-18 18:12:15 -0600

    add ImageList React component

--- imgclassify-front/src/App.js
+++ imgclassify-front/src/App.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import './App.css';
 import Classifier from './components/Classifier/Classifier';
+import ImageLIst from './components/ImageList/ImageList';
+import Navigation from './components/Navigation/Navigation';
+import {Route, BrowserRouter, Switch} from 'react-router-dom'
 
 function App() {
   return (
+    <BrowserRouter>
+      <Navigation />
       <div className='App'>
-     
-     <Classifier/>
-      
+        <Switch>
+          <Route exact path='/' component={Classifier} />
+          <Route exact path='/list' component={ImageLIst} />
+          <Route exact path='*' component={Classifier} />
+        </Switch>
       </div>
-   
+    </BrowserRouter>
   );
 }
 
